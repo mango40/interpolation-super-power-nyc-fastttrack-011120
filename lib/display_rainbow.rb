70aabbd20af8163343fad4d7992ab9b161797cb0
Manoj Patel <nycdays300@gmail.com>
2020-01-12 18:35:07 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,7 @@
 # Write your #display_rainbow method here
+
+def display_rainbow
+display_rainbow ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+end
+
+print display_rainbow 0=R, 1=0, 2=Y, 3=G, 4=B, 5=I, 6=V 
